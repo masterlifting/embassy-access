bec7c648056ecac98459603a3e0356f4028ed7a8
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-04-11 19:34:59 +0200

    Add submodules for fsharp-infrastructure and fsharp-worker

--- src/worker/Program.fs
+++ src/worker/Program.fs
@@ -1,2 +1,24 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿[<EntryPoint>]
+let main args =
+
+    match Repository.getTasks () with
+    | Error error -> $"Error: {error}" |> Log.error
+    | Ok tasks ->
+
+        let duration =
+            match args.Length with
+            | 1 ->
+                match args.[0] with
+                | DSL.AP.IsFloat seconds -> seconds
+                | _ -> (System.TimeSpan.FromDays 1).TotalSeconds
+            | _ -> (System.TimeSpan.FromDays 1).TotalSeconds
+
+        let config: Domain.Core.WorkerConfiguration =
+            { Duration = duration
+              Tasks = tasks
+              Handlers = TaskStepHandlers.taskHandlers
+              getTask = Repository.getTask }
+
+        config |> Core.startWorker |> Async.RunSynchronously
+
+    0
