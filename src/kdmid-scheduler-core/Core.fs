25cb8237c980db8b0beaec04e1110b32755f7110
Andrei Pestunov <masterlifting@gmail.com>
2024-04-20 16:58:47 +0200

    Refactor module names and types in kdmid-scheduler-core and kdmid-scheduler-worker

--- src/kdmid-scheduler-core/Core.fs
+++ src/kdmid-scheduler-core/Core.fs
@@ -10,31 +10,22 @@ let getUserCredentials' city persistenceType =
 
             Persistence.Scope.remove scope
 
-            let userCredentials =
-                Map
-                    [ { Id = UserId "1"; Name = "John" },
-                      Set
-                          [ { Id = KdmidCredentialId "1"
-                              Cd = KdmidCredentialCd "1"
-                              Ems = KdmidCredentialEms(Some "1") }
-                            { Id = KdmidCredentialId "2"
-                              Cd = KdmidCredentialCd "2"
-                              Ems = KdmidCredentialEms None } ]
-                      { Id = UserId "2"; Name = "Jane" },
-                      Set
-                          [ { Id = KdmidCredentialId "3"
-                              Cd = KdmidCredentialCd "3"
-                              Ems = KdmidCredentialEms None }
-                            { Id = KdmidCredentialId "4"
-                              Cd = KdmidCredentialCd "4"
-                              Ems = KdmidCredentialEms(Some "4") } ] ]
+            match Kdmid.createCredentials (Kdmid.Id "1") (Kdmid.Cd "1") (Kdmid.Ems(Some "1")) with
+            | Error error -> return Error error
+            | Ok kdmidCredentials ->
+
+                let userCredentials =
+                    Map
+                        [ { Id = UserId "1"; Name = "John" }, Set [ kdmidCredentials ]
+                          { Id = UserId "2"; Name = "Jane" }, Set [ kdmidCredentials ] ]
 
 
-            return Ok userCredentials
+                return Ok userCredentials
     }
 
 let getUserCredentials city =
     getUserCredentials' city Persistence.InMemoryStorage
+
 
 let getAvailableDates cityOrder : Async<Result<CityOrderResult, string>> =
     async
--- src/kdmid-scheduler-core/Domain.fs
+++ src/kdmid-scheduler-core/Domain.fs
@@ -9,17 +9,63 @@ module Core =
     type UserId = UserId of string
     type User = { Id: UserId; Name: string }
 
-    type KdmidCredentialId = KdmidCredentialId of string
-    type KdmidCredentialCd = KdmidCredentialCd of string
-    type KdmidCredentialEms = KdmidCredentialEms of string option
+    module Kdmid =
+        type Id = Id of string
+        type Cd = Cd of string
+        type Ems = Ems of string option
 
-    type KdmidCredentials =
-        { Id: KdmidCredentialId
-          Cd: KdmidCredentialCd
-          Ems: KdmidCredentialEms }
+        type Id' = private Id' of int
+        type Cd' = private Cd' of string
+        type Ems' = private Ems' of string option
 
-    type UserCredentials = Map<User, Set<KdmidCredentials>>
-    type CityCredentials = Map<City, Set<KdmidCredentials>>
+        type Credentials = { Id: Id'; Cd: Cd'; Ems: Ems' }
+
+        let createCredentials id cd ems =
+            match id, cd, ems with
+            | Id id, Cd cd, Ems ems ->
+                match id with
+                | Infrastructure.DSL.AP.IsInt id ->
+                    match cd with
+                    | Infrastructure.DSL.AP.IsLettersOrNumbers cd ->
+                        match ems with
+                        | None ->
+                            Ok
+                                { Id = Id' id
+                                  Cd = Cd' cd
+                                  Ems = Ems' None }
+                        | Some ems ->
+                            match ems with
+                            | Infrastructure.DSL.AP.IsLettersOrNumbers ems ->
+                                Ok
+                                    { Id = Id' id
+                                      Cd = Cd' cd
+                                      Ems = Ems'(Some ems) }
+                            | _ -> Error "Invalid EMS"
+                    | _ -> Error "Invalid CD"
+                | _ -> Error "Invalid ID"
+
+        let createUrlParams credentials =
+            match credentials with
+            | { Id = Id' id
+                Cd = Cd' cd
+                Ems = Ems' ems } ->
+                match ems with
+                | Some ems -> $"id={id}&cd={cd}&ems={ems}"
+                | None -> $"id={id}&cd={cd}"
+
+        let private getCityCode city =
+            match city with
+            | Belgrade -> "belgrad"
+            | Budapest -> "budapest"
+            | Sarajevo -> "sarajevo"
+
+        let createBaseUrl city =
+            let cityCode = getCityCode city
+            $"https://{cityCode}.kdmid.ru/queue/"
+
+
+    type UserCredentials = Map<User, Set<Kdmid.Credentials>>
+    type CityCredentials = Map<City, Set<Kdmid.Credentials>>
 
     type UserOrder =
         { User: User
