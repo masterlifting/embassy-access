8fcdf4e6a746491b417a1c408c0ac92414e94633
Andrei Pestunov <masterlifting@gmail.com>
2024-05-17 22:48:29 +0200

    Refactor embassy appointment scheduler configuration

--- src/eas-worker/Program.fs
+++ src/eas-worker/Program.fs
@@ -4,15 +4,9 @@ open KdmidScheduler.Worker
 [<EntryPoint>]
 let main _ =
     Logging.useConsoleLogger Configuration.AppSettings
-    
-    //TODO: Remove this line
-    //do! KdmidScheduler.Core.createTestUserKdmidOrderForCity Belgrade
 
-    Core.configure
-    |> Worker.Core.start
-    |> Async.RunSynchronously
+    Core.configure |> Worker.Core.start |> Async.RunSynchronously
 
-    Async.Sleep (System.TimeSpan.FromSeconds 0.5)
-    |> Async.RunSynchronously
+    Async.Sleep(System.TimeSpan.FromSeconds 300) |> Async.RunSynchronously
 
     0
