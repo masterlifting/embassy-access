a81c47466a805ccc23bb376ab5b5119b92bdeee0
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-05-20 18:44:45 +0200

    Refactor embassy appointment scheduler configuration

--- src/eas-worker/Core.fs
+++ src/eas-worker/Core.fs
@@ -4,29 +4,42 @@ open Infrastructure.Domain.Graph
 open Worker.Domain.Core
 
 module private WorkerHandlers =
+    open System.Threading
+
     module Serbia =
+
+        let handleRusianEmbassy (cts: CancellationTokenSource) = async { return Ok "That's all" }
+
         let BelgradeSteps =
             [ Node(
                   { Name = "Belgrade"; Handle = None },
                   [ Node(
                         { Name = "RussianEmbassy"
-                          Handle = None },
+                          Handle = Some handleRusianEmbassy },
                         [ Node(
                               { Name = "GetAvailableDates"
-                                Handle = None },
+                                Handle = Some handleRusianEmbassy },
                               []
                           )
-                          Node({ Name = "NotifyUsers"; Handle = None }, []) ]
+                          Node(
+                              { Name = "NotifyUsers"
+                                Handle = Some handleRusianEmbassy },
+                              []
+                          ) ]
                     )
                     Node(
                         { Name = "HungarianEmbassy"
-                          Handle = None },
+                          Handle = Some handleRusianEmbassy },
                         [ Node(
                               { Name = "GetAvailableDates"
-                                Handle = None },
+                                Handle = Some handleRusianEmbassy },
                               []
                           )
-                          Node({ Name = "NotifyUsers"; Handle = None }, []) ]
+                          Node(
+                              { Name = "NotifyUsers"
+                                Handle = Some handleRusianEmbassy },
+                              []
+                          ) ]
                     ) ]
               ) ]
 
@@ -35,12 +48,15 @@ open WorkerHandlers
 
 let configure () =
     async {
-        match! Repository.getWorkerTasks () with
+        let workerName = "EmbassiesAppointmentsScheduler"
+
+        match! Repository.getWorkerTasks workerName with
         | Error error -> return Error error
         | Ok tasks ->
             return
                 Ok
-                <| { getSchedule = Repository.getTaskSchedule
+                <| { Name = workerName
+                     getSchedule = Repository.getTaskSchedule workerName
                      Tasks = tasks
                      Handlers = [ Node({ Name = "Serbia"; Handle = None }, Serbia.BelgradeSteps) ] }
     }
--- src/eas-worker/Repository.fs
+++ src/eas-worker/Repository.fs
@@ -1,22 +1,21 @@
 module internal KdmidScheduler.Worker.Repository
 
-[<Literal>]
-let private WorkerSectionName = "EmbassiesAppointmentsScheduler"
 
-let getWorkerTasks () =
+let getWorkerTasks workerName =
     async {
-        match Configuration.getSection<Worker.Domain.Persistence.Task array> WorkerSectionName with
-        | None -> return Error $"Section '%s{WorkerSectionName}' was not found."
-        | Some tasks when tasks.Length = 0 -> return Error $"Section '%s{WorkerSectionName}' is empty."
+        match Configuration.getSection<Worker.Domain.Persistence.Task array> workerName with
+        | None -> return Error $"Section '%s{workerName}' was not found."
+        | Some tasks when tasks.Length = 0 -> return Error $"Section '%s{workerName}' is empty."
         | Some tasks -> return Ok <| Worker.Mapper.mapTasks tasks
     }
 
-let getTaskSchedule name =
-    async {
-        match! getWorkerTasks () with
-        | Error error -> return Error error
-        | Ok tasks ->
-            match Infrastructure.DSL.Graph.findNode' name tasks with
-            | Some task -> return Ok task.Schedule
-            | None -> return Error $"Task '{name}' was not found in the section '{WorkerSectionName}'."
+let getTaskSchedule workerName =
+    fun taskName ->
+        async {
+            match! getWorkerTasks workerName with
+            | Error error -> return Error error
+            | Ok tasks ->
+                match Infrastructure.DSL.Graph.findNode' taskName tasks with
+                | Some task -> return Ok task.Schedule
+                | None -> return Error $"Task '{taskName}' was not found in the section '{workerName}'."
     }
