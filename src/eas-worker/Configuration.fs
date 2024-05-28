3c151c7a60d3bf566ccdb37e0784dc91a79fa7f7
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-05-28 20:05:34 +0200

    chore: Update eas-worker configuration to use YAML format

--- src/eas-worker/Configuration.fs
+++ src/eas-worker/Configuration.fs
@@ -2,5 +2,5 @@ module internal Eas.Worker.Configuration
 
 open Infrastructure
 
-let AppSettings = Configuration.getJsonConfiguration "appsettings.json"
+let AppSettings = Configuration.getYamlConfiguration "appsettings.yaml"
 let getSection<'a> = AppSettings |> Configuration.getSection<'a>
