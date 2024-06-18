3229a3b3e5e1542315627c8ed4bc169e94620036
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-06-18 16:08:13 +0200

    Refactor embassy appointment scheduler configuration

--- src/eas-worker/Embassies.fs
+++ src/eas-worker/Embassies.fs
@@ -9,13 +9,26 @@ open Eas.Domain.Internal
 open Eas.Persistence.Filter
 
 module Russian =
+    let private createTestRequest storage country ct =
+        async {
+            let request =
+                { Id = System.Guid.NewGuid() |> RequestId
+                  User = { Id = UserId 1; Name = "Andrei" }
+                  Embassy = Russian country
+                  Data =
+                    Map [ "url", "https://sarajevo.kdmid.ru/queue/orderinfo.aspx?id=20781&cd=f23cb539&ems=143F4DDF" ]
+                  Modified = System.DateTime.UtcNow }
+
+            return! Eas.Persistence.Repository.Command.Request.create storage request ct
+        }
+
     let private getAvailableDates country =
         fun ct ->
             Persistence.Core.createStorage InMemory
             |> Result.mapError Infrastructure
             |> ResultAsync.wrap (fun storage ->
 
-                let requestsFilter =
+                let filter =
                     Request.ByEmbassy(
                         { Pagination =
                             { Page = 1
@@ -36,36 +49,17 @@ module Russian =
                 let setResponse response =
                     Eas.Core.Russian.setResponse storage response ct
 
-                async {
+                let _ = createTestRequest storage country ct |> Async.RunSynchronously
 
-                    //TODO: remove this block after the development
-                    let createRequest request =
-                        Eas.Persistence.Repository.Command.Request.create storage request ct
-
-                    let testRequest =
-                        { Id = System.Guid.NewGuid() |> RequestId
-                          User = { Id = UserId 0; Name = "Andrei" }
-                          Embassy = Russian country
-                          Data =
-                            Map
-                                [ "url",
-                                  "https://sarajevo.kdmid.ru/queue/orderinfo.aspx?id=20781&cd=f23cb539&ems=143F4DDF" ]
-                          Modified = System.DateTime.UtcNow }
-
-                    let! _ = createRequest testRequest
-                    //
-
-                    match! getRequests requestsFilter with
-                    | Error error -> return Error error
-                    | Ok requests ->
-                        match! tryGetResponse requests with
-                        | Error error -> return Error error
-                        | Ok None -> return Ok <| Info "No data."
-                        | Ok(Some response) ->
-                            match! setResponse response with
-                            | Error error -> return Error error
-                            | _ -> return Ok <| Success response.Appointments
-                })
+                getRequests filter
+                |> ResultAsync.bind' (
+                    tryGetResponse
+                    >> ResultAsync.bind' (fun response ->
+                        match response with
+                        | None -> async { return Ok <| Info "No data." }
+                        | Some response ->
+                            setResponse response |> ResultAsync.map (fun _ -> Success response.Appointments))
+                ))
 
     let createNode country =
         Node(
--- src/eas-worker/Persistence.fs
+++ src/eas-worker/Persistence.fs
@@ -7,26 +7,24 @@ open Infrastructure.Domain.Errors
 let private sectionName = "Worker"
 
 let private getTasksGraph handlersGraph configuration =
-    async {
-        return
-            match Configuration.getSection<Worker.Domain.External.Task> configuration sectionName with
-            | None ->
-                Error
-                <| Persistence $"Section '%s{sectionName}' was not found in the configuration."
-            | Some graph -> Worker.Mapper.buildCoreGraph graph handlersGraph |> Result.mapError Persistence
-    }
+    Configuration.getSection<Worker.Domain.External.Task> configuration sectionName
+    |> Option.map (fun graph -> Worker.Mapper.buildCoreGraph graph handlersGraph |> Result.mapError Persistence)
+    |> Option.defaultValue (
+        Error
+        <| Persistence $"Section '%s{sectionName}' was not found in the configuration."
+    )
 
 let getTaskNode handlersGraph configuration =
     fun taskName ->
         async {
-            match! getTasksGraph handlersGraph configuration with
-            | Error error -> return Error error
-            | Ok graph ->
-                return
-                    match Dsl.Graph.findNode taskName graph with
-                    | Some node -> Ok node
-                    | None ->
+            return
+                getTasksGraph handlersGraph configuration
+                |> Result.bind (fun graph ->
+                    Dsl.Graph.findNode taskName graph
+                    |> Option.map Ok
+                    |> Option.defaultValue (
                         Error
                         <| Persistence
-                            $"Task '{taskName}' was not found in the section '{sectionName}' of the configuration."
+                            $"Task '%s{taskName}' was not found in the section '%s{sectionName}' of the configuration."
+                    ))
         }
