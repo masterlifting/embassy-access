9e6566a96668dd78fb0f62529f98bcd34da7a9f5
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-06-30 00:10:48 +0200

    Refactor embassy appointment scheduler configuration

---
+++ tests/eas-core/Core.fs
@@ -0,0 +1,15 @@
+﻿module Eas.Core.Tests
+open Expecto
+
+module Russian =
+    
+    let private getResponse =
+        let name = "Test"
+        testTask name {
+            Expect.equal "Test" name "Values should be equal"
+        }
+
+    let tests =
+        testList "Russian" [
+            getResponse
+        ]
---
+++ tests/eas-core/Program.fs
@@ -0,0 +1,12 @@
+﻿module Eas.Program.Tests
+
+open Expecto
+open Eas.Core.Tests
+
+[<Tests>]
+let tests =
+    testList "all"
+    [ Russian.tests ]
+
+[<EntryPoint>]
+let main args = runTestsWithCLIArgs [] args tests
