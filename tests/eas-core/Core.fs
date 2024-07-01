ce3c19bc4a727bae9cd5ac8a620fc5ac79da82ab
Andrei Pestunov <masterlifting@gmail.com>
2024-07-01 22:49:52 +0200

    Refactor embassy appointment scheduler configuration

--- tests/eas-core/Core.fs
+++ tests/eas-core/Core.fs
@@ -2,13 +2,53 @@
 
 open Expecto
 
-module Russian =
-    open Eas.Core.Russian
+module Embassies =
 
-    let private ``get kdmid response`` =
-        let name = "Get kdmid response"
-        testAsync name { 
-            //let! response = getResponse
-            Expect.equal "Get kdmid response" name "Values should be equal" }
+    module Russian =
+        open System
+        open Infrastructure.Dsl
+        open Persistence
+        open Persistence.Core
+        open Persistence.Domain
+        open Eas.Persistence
+        open Eas.Core
+        open Eas.Domain.Internal
 
-    let tests = testList "Russian.Kdmid" [ ``get kdmid response`` ]
+        let private ``first page should be parsed`` =
+            testAsync "First page should be parsed" {
+                let filesPath = Environment.CurrentDirectory + "/test_data"
+                let startPage = Domain.FileSystem(filesPath + "/start_page_response.html")
+
+                let result =
+                    createStorage startPage
+                    |> ResultAsync.wrap (fun storage ->
+                        async {
+                            match! storage |> Storage.FileSystem.get with
+                            | Error e -> return Error e
+                            | Ok page ->
+                                let request: Request =
+                                    { Id = Guid.NewGuid() |> RequestId
+                                      User = { Id = UserId 1; Name = "Andrei" }
+                                      Embassy = Russian <| Serbia Belgrade
+                                      Data =
+                                        Map
+                                            [ "url",
+                                              "https://sarajevo.kdmid.ru/queue/orderinfo.aspx?id=20781&cd=f23cb539&ems=143F4DDF" ]
+                                      Modified = DateTime.UtcNow }
+
+                                return!
+                                    request
+                                    |> Russian.getResponse
+                                        { getStartPage = fun _ _ -> async { return Ok page }
+                                          postValidationPage = fun _ _ _ -> async { return Ok String.Empty }
+                                          getCaptchaImage = fun _ _ -> async { return Ok [||] }
+                                          solveCaptchaImage = fun _ -> async { return Ok 1 } }
+                        })
+
+                match! result with
+                | Ok _ -> return ()
+                | Error e -> failwith e
+
+            }
+
+        let tests = testList "Russian.Kdmid" [ ``first page should be parsed`` ]
--- tests/eas-core/Program.fs
+++ tests/eas-core/Program.fs
@@ -1,9 +1,9 @@
-﻿module Eas.Program.Tests
+﻿module Eas.Core.Program
 
 open Expecto
 open Eas.Core.Tests
 
-let private tests = testList "Tests" [ Russian.tests ]
+let private tests = testList "Tests" [ Embassies.Russian.tests ]
 
 [<EntryPoint>]
 let main args = runTestsWithCLIArgs [] args tests
