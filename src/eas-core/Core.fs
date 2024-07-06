730ecaa19f820d0b8d5da32bb15a6d64facf53fd
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-07-06 17:36:56 +0200

    Refactor embassy appointment scheduler configuration

--- src/eas-core/Core.fs
+++ src/eas-core/Core.fs
@@ -15,19 +15,18 @@ module Russian =
         let headers =
             Map
                 [ "Accept",
-                  "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-                  "Accept-Language", "en-US,en;q=0.9,ru;q=0.8"
-                  
-                  "Cache-Control", "max-age=0"
-                  "Sec-Ch-Ua", "Not A(Brand\";v=\"99\", \"Microsoft Edge\";v=\"121\", \"Chromium\";v=\"121"
-                  "Sec-Ch-Ua-Mobile", "?0"
-                  "Sec-Ch-Ua-Platform", "\"Windows\""
-                  "Sec-Fetch-Dest", "document"
-                  "Sec-Fetch-Mode", "navigate"
-                  "Sec-Fetch-User", "?1"
-                  "Upgrade-Insecure-Requests", "1"
+                  [ "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" ]
+                  "Accept-Language", [ "en-US,en;q=0.9,ru;q=0.8" ]
+                  "Cache-Control", [ "max-age=0" ]
+                  "Sec-Ch-Ua", [ "Not A(Brand\";v=\"99\", \"Microsoft Edge\";v=\"121\", \"Chromium\";v=\"121" ]
+                  "Sec-Ch-Ua-Mobile", [ "?0" ]
+                  "Sec-Ch-Ua-Platform", [ "\"Windows\"" ]
+                  "Sec-Fetch-Dest", [ "document" ]
+                  "Sec-Fetch-Mode", [ "navigate" ]
+                  "Sec-Fetch-User", [ "?1" ]
+                  "Upgrade-Insecure-Requests", [ "1" ]
                   "User-Agent",
-                  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0" ]
+                  [ "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0" ] ]
             |> Some
 
         Http.create $"https://%s{city}.kdmid.ru" headers
@@ -37,29 +36,27 @@ module Russian =
         | Some ems -> $"id=%i{id}&cd=%s{cd}&ems=%s{ems}"
         | None -> $"id=%i{id}&cd=%s{cd}"
 
-    let private buildFormData data =
+    let private buildFormData (data: (string * string) list) =
         data
-        |> Map.add "__EVENTTARGET" String.Empty
-        |> Map.add "__EVENTARGUMENT" String.Empty
-        |> Seq.map (fun x -> $"{Uri.EscapeDataString x.Key}={Uri.EscapeDataString x.Value}")
+        |> Seq.map (fun (key, value) -> $"{Uri.EscapeDataString key}={Uri.EscapeDataString value}")
         |> String.concat "&"
 
-    let private setCookie data httpClient =
-        let headers = data |> String.concat "; " |> (fun x -> Map [ "Cookie", x ])
+    let private setCookie cookies httpClient =
+        let headers = Map [ "Cookie", cookies ]
         httpClient |> Http.Headers.add (Some headers)
 
     let private setRequiredCookie httpClient (data: string, headers: Headers) =
         headers
         |> Http.Headers.find "Set-Cookie" [ "AlteonP"; "__ddg1_" ]
-        |> Result.map (fun result ->
-            httpClient |> setCookie result
+        |> Result.map (fun cookie ->
+            httpClient |> setCookie cookie
             data)
 
     let private setSessionCookie httpClient (image: byte array, headers: Headers) =
         headers
         |> Http.Headers.find "Set-Cookie" [ "ASP.NET_SessionId" ]
-        |> Result.map (fun result ->
-            httpClient |> setCookie result
+        |> Result.map (fun cookie ->
+            httpClient |> setCookie cookie
             image)
 
     let private getStartPageData getStartPage getCaptchaImage solveCaptchaImage httpClient =
@@ -76,39 +73,63 @@ module Russian =
                 match pageData |> Map.tryFind "captcha" with
                 | None -> async { return Error <| NotFound "Captcha information on the Start Page." }
                 | Some urlPath ->
+                    let origin = httpClient |> Http.Route.toOrigin
+                    let host = httpClient |> Http.Route.toHost
+
+                    let headers =
+                        Map
+                            [ "Host", [ host ]
+                              "Referer", [ (origin + "/queue/orderinfo.aspx?" + queryParams) ]
+                              "Sec-Fetch-Site", [ "same-origin" ] ]
+
                     let request =
                         { Path = $"/queue/{urlPath}"
-                          Headers = None }
+                          Headers = Some headers }
 
                     let addFormData captcha =
                         pageData
                         |> Map.remove "captcha"
                         |> Map.add "ctl00$MainContent$txtCode" $"%i{captcha}"
+                        |> Map.add "__EVENTTARGET" ""
+                        |> Map.add "__EVENTARGUMENT" ""
                         |> Map.add "ctl00$MainContent$FeedbackClientID" "0"
                         |> Map.add "ctl00$MainContent$FeedbackOrderID" "0"
+
+                    let formatDataOrderPattern =
+                        [ "__EVENTTARGET"
+                          "__EVENTARGUMENT"
+                          "__VIEWSTATE"
+                          "__VIEWSTATEGENERATOR"
+                          "__EVENTVALIDATION"
+                          "ctl00$MainContent$txtID"
+                          "ctl00$MainContent$txtUniqueID"
+                          "ctl00$MainContent$txtCode"
+                          "ctl00$MainContent$ButtonA"
+                          "ctl00$MainContent$FeedbackClientID"
+                          "ctl00$MainContent$FeedbackOrderID" ]
+
+                    let orderFormData (pattern: string list) (pageData: Map<string, string>) : (string * string) list =
+
+                        let a = pattern |> List.map (fun key -> key, pageData[key])
+
+                        a
+
 
                     httpClient
                     |> getCaptchaImage request
                     |> ResultAsync.bind (httpClient |> setSessionCookie)
                     |> ResultAsync.bind' solveCaptchaImage
                     |> ResultAsync.map' addFormData
+                    |> ResultAsync.map' (orderFormData formatDataOrderPattern)
                     |> ResultAsync.map' buildFormData)
 
     let private getValidationPageData postValidationPage httpClient =
         fun formData queryParams ->
 
-            let requestPath = "/queue/orderinfo.aspx?" + queryParams
-            let origin = httpClient |> Http.Route.toOrigin
-
-            let headers =
-                Map
-                    [ "Origin", origin
-                      "Referer", (origin + requestPath)
-                      "Priority", "u=0, i"
-                      "Sec-Fetch-Site", "same-origin" ]
+            let headers = Map [ "Origin", [ httpClient |> Http.Route.toOrigin ] ]
 
             let request =
-                { Path = requestPath
+                { Path = "/queue/orderinfo.aspx?" + queryParams
                   Headers = Some headers }
 
             let content =
@@ -123,6 +144,7 @@ module Russian =
                 |> Map.add "ctl00$MainContent$ButtonA.y" "20"
                 |> Map.add "ctl00$MainContent$FeedbackClientID" "0"
                 |> Map.add "ctl00$MainContent$FeedbackOrderID" "0"
+                |> Map.toList
 
             httpClient
             |> postValidationPage request content
