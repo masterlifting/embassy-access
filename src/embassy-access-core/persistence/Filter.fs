ab98c5dbfe86117698b5def634f710c779c6a04b
Andrey Pestunov <masterlifting@gmail.com>
2024-09-03 03:02:05 +0200

    Refactor filtering and request handling logic
    - Change types in `Filter.fs` from public to internal
    - Replace `Request` type with discriminated union in `Filter.fs`
    - Refactor `paginate` in `InMemoryRepository.fs` to require `Pagination`
    - Add new filter functions in `Request.Filters` module
    - Update `get` function in `Request` module to use new filters
    - Add private helper functions in `Russian.fs`
    - Update `SearchAppointments` and `MakeAppointments` modules to use new filters
    - Refactor `processRequests` to handle grouped requests asynchronously
    - Update `run` functions to use new filtering and processing logic
    - Update `addTasks` to use new `run` function for `MakeAppointments`

--- src/embassy-access-core/persistence/Filter.fs
+++ src/embassy-access-core/persistence/Filter.fs
@@ -4,42 +4,31 @@ module EmbassyAccess.Persistence.Filter
 open System
 open EmbassyAccess
 
-type OrderBy<'a> =
+type internal OrderBy<'a> =
     | Date of ('a -> DateTime)
     | String of ('a -> string)
     | Int of ('a -> int)
     | Bool of ('a -> bool)
     | Guid of ('a -> Guid)
 
-type SortBy<'a> =
+type internal SortBy<'a> =
     | Asc of OrderBy<'a>
     | Desc of OrderBy<'a>
 
-type predicate<'a> = 'a -> bool
+type internal predicate<'a> = 'a -> bool
 
-type Pagination<'a> =
+type internal Pagination<'a> =
     { Page: int
       PageSize: int
       SortBy: SortBy<'a> }
 
-type Request =
-    { Pagination: Pagination<Domain.Request> option
-      Ids: Domain.RequestId Set option
-      Embassies: Domain.Embassy Set option
-      HasAppointments: bool option
-      HasConfirmations: bool option
-      HasConfirmationState: predicate<Domain.ConfirmationState> option
-      HasStates: predicate<Domain.RequestState> option
-      WasModified: predicate<DateTime> option }
-
-type SearchAppointmentsRequest =
+type internal SearchAppointmentsRequest =
     { Pagination: Pagination<Domain.Request>
       Embassy: Domain.Embassy
       HasStates: predicate<Domain.RequestState>
-      HasConfirmationState: predicate<Domain.ConfirmationState>
-      GroupBy: string }
+      HasConfirmationState: predicate<Domain.ConfirmationState> }
 
-    static member Create(embassy: Domain.Embassy) =
+    static member create embassy =
         { Pagination =
             { Page = 1
               PageSize = 20
@@ -52,11 +41,29 @@ type SearchAppointmentsRequest =
           HasConfirmationState =
             function
             | Domain.Auto _ -> false
-            | _ -> true
-          GroupBy = "" }
+            | _ -> true }
 
-type MakeAppointmentRequest =
+type internal MakeAppointmentRequest =
     { Pagination: Pagination<Domain.Request>
       Embassy: Domain.Embassy
       HasStates: predicate<Domain.RequestState>
-      HasConfirmationState: predicate<Domain.ConfirmationState> }
+      HasConfirmationStates: predicate<Domain.ConfirmationState> }
+
+      static member create embassy =
+        { Pagination =
+            { Page = 1
+              PageSize = 20
+              SortBy = OrderBy<Domain.Request>.Date _.Modified |> Asc }
+          Embassy = embassy
+          HasStates =
+            function
+            | Domain.InProcess -> true
+            | _ -> false
+          HasConfirmationStates =
+            function
+            | Domain.Auto _ -> true
+            | _ -> false }
+
+type Request =
+    | SearchAppointments of Domain.Embassy
+    | MakeAppointments of Domain.Embassy
