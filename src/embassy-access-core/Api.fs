bb6ce072a6820e2cf7ce7eefb1d69158591c1aa9
Andrei Pestunov <masterlifting@gmail.com>
2024-09-05 23:39:31 +0200

    Add Telegram bot support and refactor client modules
    - Added `notifySubscribers` and `notifySubscriber` functions in `EmbassyAccess.Api` module.
    - Enhanced `Russian` module to create and use `telegramBot` for processing requests.
    - Updated `SearchAppointments` and `MakeAppointments` to use `bot` for notifications.
    - Simplified error handling in `choose` function of `SearchAppointments`.
    - Introduced `Web.Domain` module to define `Context` type (`Http` or `Telegram`).
    - Added `Web.Client` module to create clients based on `Context` type.
    - Moved `Client` type and related types in `Web.Telegram.Domain` to main module.
    - Updated `fsharp-web.fsproj` to include new files and `Telegram.Bot` package.
    - Refined `Web.Client` module to support `Http` and `Telegram` clients.

--- src/embassy-access-core/Api.fs
+++ src/embassy-access-core/Api.fs
@@ -6,3 +6,7 @@ type ProcessRequestDeps = Russian of Embassies.Russian.Domain.ProcessRequestDeps
 let processRequest deps request =
     match deps with
     | Russian deps -> request |> Embassies.Russian.Core.processRequest deps
+
+let notifySubscribers embassy appointments client = async { return Ok() }
+
+let notifySubscriber requestId confirmations client = async { return Ok() }
