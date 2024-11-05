a00d5f42005d3c2afaa0dda9ed1c7afd86558490
Andrey Pestunov <masterlifting@gmail.com>
2024-11-05 01:13:37 +0100

    Add logging for PID file location and new PID value in deployment script

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,6 +5,9 @@ set -e
 cd /usr/src/embassy-access/src/embassy-access-worker/
 
 PID_FILE="/tmp/embassy-access-pid"
+
+echo "PID file is located at: $PID_FILE"
+
 if [[ -f "$PID_FILE" ]]; then
     LAST_PID=$(cat "$PID_FILE")
     if kill -0 $LAST_PID 2>/dev/null; then
@@ -14,5 +17,6 @@ fi
 
 nohup dotnet run -c Release > /dev/null 2>&1 &
 echo $! > "$PID_FILE"
+echo "New PID value is: $(cat "$PID_FILE")"
 
 exit 0
