c1506eecdcdc1309cfedf0f44867a146fc5f3f47
Andrei Pestunov <masterlifting@gmail.com>
2024-11-26 22:17:29 +0100

    Refactor embassy access worker configuration: update file system path, streamline task definitions, and enhance worker routing structure

--- src/embassy-access-worker/embassies/Russian.fs
+++ src/embassy-access-worker/embassies/Russian.fs
@@ -5,6 +5,35 @@ open Infrastructure
 open Worker.Domain
 open EA.Core.Domain
 
+type WorkerRoute =
+    | Name of string
+
+    interface Graph.INodeName with
+        member this.Id = Graph.NodeId.New
+
+        member this.Name =
+            match this with
+            | Name name -> name
+
+        member this.setName name = Name name
+
+let private WORKER_ROUTER =
+    Graph.Node(
+        Name "Russian",
+        [ Graph.Node(Name "Serbia", [ Graph.Node(Name "Belgrade", []) ])
+          Graph.Node(Name "Germany", [ Graph.Node(Name "Berlin", []) ])
+          Graph.Node(Name "France", [ Graph.Node(Name "Paris", []) ])
+          Graph.Node(Name "Montenegro", [ Graph.Node(Name "Podgorica", []) ])
+          Graph.Node(Name "Ireland", [ Graph.Node(Name "Dublin", []) ])
+          Graph.Node(Name "Switzerland", [ Graph.Node(Name "Bern", []) ])
+          Graph.Node(Name "Finland", [ Graph.Node(Name "Helsinki", []) ])
+          Graph.Node(Name "Netherlands", [ Graph.Node(Name "Hague", []) ])
+          Graph.Node(Name "Albania", [ Graph.Node(Name "Tirana", []) ])
+          Graph.Node(Name "Slovenia", [ Graph.Node(Name "Ljubljana", []) ])
+          Graph.Node(Name "Bosnia", [ Graph.Node(Name "Sarajevo", []) ])
+          Graph.Node(Name "Hungary", [ Graph.Node(Name "Budapest", []) ]) ]
+    )
+
 module internal Kdmid =
     open EA.Embassies.Russian.Kdmid.Domain
 
@@ -70,3 +99,11 @@ let register () =
               []
           ) ]
     )
+    
+let register'() =
+    let rec innerLoop (node: Graph.Node<WorkerRoute>) =
+        let handler =
+            match node.Value with
+            | Name "Search appointments" -> Kdmid.SearchAppointments.run ()
+    
+    WORKER_ROUTER |> innerLoop
