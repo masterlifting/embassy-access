c77fb2122907dbb9c63cbde06792da37e282fea8
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-11-26 17:41:35 +0100

    Refactor embassy access worker settings: update section name to use AppName, streamline task graph configuration retrieval, and remove unused schedule function

--- src/embassy-access-worker/Settings.fs
+++ src/embassy-access-worker/Settings.fs
@@ -6,31 +6,16 @@ open Worker.Domain
 [<Literal>]
 let AppName = "Worker"
 
-[<Literal>]
-let private SectionName = "Worker"
-
 let getTaskGraphConfig configuration =
     configuration
-    |> Configuration.getSection<External.TaskGraph> SectionName
+    |> Configuration.getSection<External.TaskGraph> AppName
     |> Option.map Ok
-    |> Option.defaultValue (Error <| NotFound $"Section '%s{SectionName}' in the configuration.")
-
-let getSchedule configuration =
-    fun taskName ->
-        configuration
-        |> getTaskGraphConfig
-        |> Result.map (Worker.Graph.createTaskGraph)
-        |> Result.map (Graph.DFS.tryFindByName taskName >> Option.bind _.Value.Schedule)
-        |> Result.bind (
-            Option.map Ok
-            >> Option.defaultValue (Error <| NotFound $"Task schedule '%s{taskName}' in the configuration.")
-        )
+    |> Option.defaultValue (Error <| NotFound $"Section '%s{AppName}' in the configuration.")
 
 let getTask configuration workerHandlers =
     fun taskName ->
         configuration
         |> getTaskGraphConfig
-        |> Result.map (Worker.Graph.createTaskGraph)
         |> Result.bind (Worker.Graph.merge workerHandlers)
         |> Result.bind (
             Graph.DFS.tryFindByName taskName
