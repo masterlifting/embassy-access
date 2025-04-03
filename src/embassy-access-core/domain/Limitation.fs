2609e527ab39ac23f283d5ad3b743d8260591a47
Andrei Pestunov <masterlifting@gmail.com>
2025-04-03 20:34:58 +0200

    Refactor LimitationState and Check method for improved clarity and functionality

--- src/embassy-access-core/domain/Limitation.fs
+++ src/embassy-access-core/domain/Limitation.fs
@@ -5,20 +5,33 @@ open System
 open Infrastructure.Domain
 
 type LimitationState =
-    | Active of DateTime
-    | Inactive
+    | Active of TimeSpan * uint<attempts>
+    | Inactive of TimeSpan
     | Expired
-    | Reset
 
 type Limitation = {
     Limit: uint<attempts>
     Period: TimeSpan
+    State: LimitationState
 } with
 
-    member this.Check (modified: DateTime) timeZone =
+    member this.Check (date: DateTime) timeZone =
 
-        let modified = modified.AddHours timeZone
-        let today = DateTime.UtcNow.AddHours timeZone
+        let state =
+            match this.State with
+            | Active(period, limit) ->
+                let now = DateTime.UtcNow.AddHours timeZone
+                let lastModifiedDate = date.AddHours timeZone
+                let remainingPeriod = period - (now - lastModifiedDate)
+                let remainingLimit = limit - 1u<attempts>
+
+                if remainingPeriod < TimeSpan.Zero then Expired
+                else if remainingLimit = 0u<attempts> then Inactive remainingPeriod
+                else Active(remainingPeriod, remainingLimit)
+            | Expired -> Active(this.Period, this.Limit)
+            | Inactive period -> Active(period, 0u<attempts>)
+            
+
         //
         // match modified.DayOfYear = today.DayOfYear, attempt > attemptLimit with
         // | true, true ->
