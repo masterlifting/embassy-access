6f02cd077dead323f531240ab75ab3e0cdccaac7
Andrei Pestunov <masterlifting@gmail.com>
2025-10-22 21:58:30 +0200

    Refactor: Add Postgre request storage implementation and update project references

---
+++ src/embassy-access-core/data_access/request/Postrgre.fs
@@ -0,0 +1,310 @@
+module EA.Core.DataAccess.Postgre.Request
+
+open Infrastructure.Prelude
+open EA.Core.Domain
+open EA.Core.DataAccess
+open Persistence.Storages.Postgre
+open Persistence.Storages.Domain.Postgre
+
+module Query =
+
+    let getIdentifiers (client: Client) =
+        async {
+            let request = {
+                Sql = "SELECT id FROM requests"
+                Params = None
+            }
+
+            return!
+                client
+                |> Query.get<string> request
+                |> ResultAsync.bind (Seq.map RequestId.create >> Result.choose)
+        }
+
+    let tryFindById (id: RequestId) payloadConverter (client: Client) =
+        async {
+            let request = {
+                Sql =
+                    """
+                    SELECT id, service_id, service_name, service_description,
+                           embassy_id, embassy_name, embassy_description, embassy_timezone,
+                           payload, process_state, limits, created, modified
+                    FROM requests
+                    WHERE id = @Id
+                """
+                Params = Some {| Id = id.ValueStr |}
+            }
+
+            return!
+                client
+                |> Query.get<Request.Entity<_>> request
+                |> ResultAsync.map Seq.tryHead
+                |> ResultAsync.bind (Option.toResult (fun e -> e.ToDomain payloadConverter))
+        }
+
+    let findManyByEmbassyId (embassyId: EmbassyId) payloadConverter (client: Client) =
+        async {
+            let request = {
+                Sql =
+                    """
+                    SELECT id, service_id, service_name, service_description,
+                           embassy_id, embassy_name, embassy_description, embassy_timezone,
+                           payload, process_state, limits, created, modified
+                    FROM requests
+                    WHERE embassy_id = @EmbassyId
+                """
+                Params = Some {| EmbassyId = embassyId.Value |}
+            }
+
+            return!
+                client
+                |> Query.get<Request.Entity<_>> request
+                |> ResultAsync.bind (Seq.map (fun e -> e.ToDomain payloadConverter) >> Result.choose)
+        }
+
+    let findManyByServiceId (serviceId: ServiceId) payloadConverter (client: Client) =
+        async {
+            let request = {
+                Sql =
+                    """
+                    SELECT id, service_id, service_name, service_description,
+                           embassy_id, embassy_name, embassy_description, embassy_timezone,
+                           payload, process_state, limits, created, modified
+                    FROM requests
+                    WHERE service_id = @ServiceId
+                """
+                Params = Some {| ServiceId = serviceId.Value |}
+            }
+
+            return!
+                client
+                |> Query.get<Request.Entity<_>> request
+                |> ResultAsync.bind (Seq.map (fun e -> e.ToDomain payloadConverter) >> Result.choose)
+        }
+
+    let findManyStartWithServiceId (serviceId: ServiceId) payloadConverter (client: Client) =
+        async {
+            let request = {
+                Sql =
+                    """
+                    SELECT id, service_id, service_name, service_description,
+                           embassy_id, embassy_name, embassy_description, embassy_timezone,
+                           payload, process_state, limits, created, modified
+                    FROM requests
+                    WHERE service_id LIKE @ServiceIdPattern
+                """
+                Params =
+                    Some {|
+                        ServiceIdPattern = serviceId.Value + "%"
+                    |}
+            }
+
+            return!
+                client
+                |> Query.get<Request.Entity<_>> request
+                |> ResultAsync.bind (Seq.map (fun e -> e.ToDomain payloadConverter) >> Result.choose)
+        }
+
+    let findManyByIds (ids: RequestId seq) payloadConverter (client: Client) =
+        async {
+            let idArray = ids |> Seq.map _.ValueStr |> Seq.toArray
+
+            let request = {
+                Sql =
+                    """
+                    SELECT id, service_id, service_name, service_description,
+                           embassy_id, embassy_name, embassy_description, embassy_timezone,
+                           payload, process_state, limits, created, modified
+                    FROM requests
+                    WHERE id = ANY(@Ids)
+                """
+                Params = Some {| Ids = idArray |}
+            }
+
+            return!
+                client
+                |> Query.get<Request.Entity<_>> request
+                |> ResultAsync.bind (Seq.map (fun e -> e.ToDomain payloadConverter) >> Result.choose)
+                |> ResultAsync.map List.ofSeq
+        }
+
+    let findManyByEmbassyIdAndServiceId
+        (embassyId: EmbassyId)
+        (serviceId: ServiceId)
+        payloadConverter
+        (client: Client)
+        =
+        async {
+            let request = {
+                Sql =
+                    """
+                    SELECT id, service_id, service_name, service_description,
+                           embassy_id, embassy_name, embassy_description, embassy_timezone,
+                           payload, process_state, limits, created, modified
+                    FROM requests
+                    WHERE embassy_id = @EmbassyId AND service_id = @ServiceId
+                """
+                Params =
+                    Some {|
+                        EmbassyId = embassyId.Value
+                        ServiceId = serviceId.Value
+                    |}
+            }
+
+            return!
+                client
+                |> Query.get<Request.Entity<_>> request
+                |> ResultAsync.bind (Seq.map (fun e -> e.ToDomain payloadConverter) >> Result.choose)
+        }
+
+module Command =
+
+    let private toEntity (request: Request<_>) (payloadConverter: Request.PayloadConverter<_, _>) =
+        payloadConverter.toEntity request.Payload
+        |> Result.map (fun payload -> {|
+            Id = request.Id.ValueStr
+            ServiceId = request.Service.Id.Value
+            ServiceName = request.Service.NameParts |> Array.ofList
+            ServiceDescription = request.Service.Description
+            EmbassyId = request.Embassy.Id.Value
+            EmbassyName = request.Embassy.NameParts |> Array.ofList
+            EmbassyDescription = request.Embassy.Description
+            EmbassyTimeZone = request.Embassy.TimeZone
+            Payload = payload
+            ProcessState = request.ProcessState // Needs serialization
+            Limits = request.Limits // Needs serialization
+            Created = request.Created
+            Modified = request.Modified
+        |})
+
+    let create (request: Request<_>) payloadConverter (client: Client) =
+        async {
+            match toEntity request payloadConverter with
+            | Error err -> return Error err
+            | Ok entity ->
+                let sql = {
+                    Sql =
+                        """
+                        INSERT INTO requests (
+                            id, service_id, service_name, service_description,
+                            embassy_id, embassy_name, embassy_description, embassy_timezone,
+                            payload, process_state, limits, created, modified
+                        ) VALUES (
+                            @Id, @ServiceId, @ServiceName::text[], @ServiceDescription,
+                            @EmbassyId, @EmbassyName::text[], @EmbassyDescription, @EmbassyTimeZone,
+                            @Payload::jsonb, @ProcessState::jsonb, @Limits::jsonb, @Created, @Modified
+                        )
+                    """
+                    Params = Some entity
+                }
+
+                return! client |> Command.execute sql |> ResultAsync.map (fun _ -> request)
+        }
+
+    let update (request: Request<_>) payloadConverter (client: Client) =
+        async {
+            match toEntity request payloadConverter with
+            | Error err -> return Error err
+            | Ok entity ->
+                let sql = {
+                    Sql =
+                        """
+                        UPDATE requests SET
+                            service_id = @ServiceId,
+                            service_name = @ServiceName::text[],
+                            service_description = @ServiceDescription,
+                            embassy_id = @EmbassyId,
+                            embassy_name = @EmbassyName::text[],
+                            embassy_description = @EmbassyDescription,
+                            embassy_timezone = @EmbassyTimeZone,
+                            payload = @Payload::jsonb,
+                            process_state = @ProcessState::jsonb,
+                            limits = @Limits::jsonb,
+                            modified = @Modified
+                        WHERE id = @Id
+                    """
+                    Params = Some entity
+                }
+
+                return! client |> Command.execute sql |> ResultAsync.map (fun _ -> request)
+        }
+
+    let updateSeq (requests: Request<_> seq) payloadConverter (client: Client) =
+        async {
+            // For batch updates, we'll execute them sequentially
+            // Consider using transactions for better atomicity
+            let mutable results = []
+            let mutable error = None
+
+            for request in requests do
+                match error with
+                | Some _ -> ()
+                | None ->
+                    let! result = update request payloadConverter client
+                    match result with
+                    | Ok r -> results <- r :: results
+                    | Error e -> error <- Some e
+
+            return
+                match error with
+                | Some e -> Error e
+                | None -> Ok(results |> List.rev)
+        }
+
+    let createOrUpdate (request: Request<_>) payloadConverter (client: Client) =
+        async {
+            match toEntity request payloadConverter with
+            | Error err -> return Error err
+            | Ok entity ->
+                let sql = {
+                    Sql =
+                        """
+                        INSERT INTO requests (
+                            id, service_id, service_name, service_description,
+                            embassy_id, embassy_name, embassy_description, embassy_timezone,
+                            payload, process_state, limits, created, modified
+                        ) VALUES (
+                            @Id, @ServiceId, @ServiceName::text[], @ServiceDescription,
+                            @EmbassyId, @EmbassyName::text[], @EmbassyDescription, @EmbassyTimeZone,
+                            @Payload::jsonb, @ProcessState::jsonb, @Limits::jsonb, @Created, @Modified
+                        )
+                        ON CONFLICT (id) DO UPDATE SET
+                            service_id = EXCLUDED.service_id,
+                            service_name = EXCLUDED.service_name,
+                            service_description = EXCLUDED.service_description,
+                            embassy_id = EXCLUDED.embassy_id,
+                            embassy_name = EXCLUDED.embassy_name,
+                            embassy_description = EXCLUDED.embassy_description,
+                            embassy_timezone = EXCLUDED.embassy_timezone,
+                            payload = EXCLUDED.payload,
+                            process_state = EXCLUDED.process_state,
+                            limits = EXCLUDED.limits,
+                            modified = EXCLUDED.modified
+                    """
+                    Params = Some entity
+                }
+
+                return! client |> Command.execute sql |> ResultAsync.map (fun _ -> request)
+        }
+
+    let delete (id: RequestId) (client: Client) =
+        async {
+            let sql = {
+                Sql = "DELETE FROM requests WHERE id = @Id"
+                Params = Some {| Id = id.ValueStr |}
+            }
+
+            return! client |> Command.execute sql |> ResultAsync.map ignore
+        }
+
+    let deleteMany (ids: RequestId Set) (client: Client) =
+        async {
+            let idArray = ids |> Set.map _.ValueStr |> Set.toArray
+
+            let sql = {
+                Sql = "DELETE FROM requests WHERE id = ANY(@Ids)"
+                Params = Some {| Ids = idArray |}
+            }
+
+            return! client |> Command.execute sql |> ResultAsync.map ignore
+        }
--- src/embassy-access-core/data_access/request/Storage.fs
+++ src/embassy-access-core/data_access/request/Storage.fs
@@ -11,6 +11,7 @@ open EA.Core.DataAccess
 type StorageType =
     | InMemory of InMemory.Connection
     | FileSystem of FileSystem.Connection
+    | Postgre of Postgre.Connection
 
 let private toProvider =
     function
@@ -21,6 +22,13 @@ let init payloadConverter =
         match storageType with
         | FileSystem connection -> connection |> Storage.Connection.FileSystem |> Storage.init
         | InMemory connection -> connection |> Storage.Connection.InMemory |> Storage.init
+        | Postgre connection ->
+            {
+                Database.Database = Database.Postgre connection.String
+                Database.Lifetime = connection.Lifetime
+            }
+            |> Storage.Connection.Database
+            |> Storage.init
         |> Result.map (fun provider -> (provider, payloadConverter) |> Request.Storage.Provider)
 
 module Query =
@@ -39,6 +47,9 @@ module Query =
         match provider with
         | Storage.InMemory client -> client |> InMemory.Request.Query.getIdentifiers
         | Storage.FileSystem client -> client |> FileSystem.Request.Query.getIdentifiers
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> Postgre.Request.Query.getIdentifiers
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
     let tryFind filter storage =
@@ -54,9 +65,15 @@ module Query =
             | Id id -> FileSystem.Request.Query.tryFindById id
             |> fun find -> client |> find payloadMapper
 
+        let postgreQuery client =
+            match filter with
+            | Id id -> Postgre.Request.Query.tryFindById id
+            |> fun find -> client |> find payloadMapper
+
         match provider with
         | Storage.InMemory client -> client |> inMemoryQuery
         | Storage.FileSystem client -> client |> fileSystemQuery
+        | Storage.Database(Database.Client.Postgre client) -> client |> postgreQuery
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
     let findMany filter storage =
@@ -82,9 +99,22 @@ module Query =
                 FileSystem.Request.Query.findManyByEmbassyIdAndServiceId embassyId serviceId
             |> fun find -> client |> find payloadMapper
 
+        let postgreQuery client =
+            match filter with
+            | Ids ids -> Postgre.Request.Query.findManyByIds ids
+            | ByEmbassyId embassyId -> Postgre.Request.Query.findManyByEmbassyId embassyId
+            | ByServiceId serviceId -> Postgre.Request.Query.findManyByServiceId serviceId
+            | StartWithServiceId serviceId -> Postgre.Request.Query.findManyStartWithServiceId serviceId
+            | ByEmbassyAndServiceId(embassyId, serviceId) ->
+                Postgre.Request.Query.findManyByEmbassyIdAndServiceId embassyId serviceId
+            |> fun find -> client |> find payloadMapper
+
         match provider with
         | Storage.InMemory client -> client |> inMemoryQuery
         | Storage.FileSystem client -> client |> fileSystemQuery
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> postgreQuery
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
 module Command =
@@ -94,6 +124,9 @@ module Command =
         match provider with
         | Storage.InMemory client -> client |> InMemory.Request.Command.create request payloadMapper
         | Storage.FileSystem client -> client |> FileSystem.Request.Command.create request payloadMapper
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> Postgre.Request.Command.create request payloadMapper
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
     let update request storage =
@@ -101,6 +134,9 @@ module Command =
         match provider with
         | Storage.InMemory client -> client |> InMemory.Request.Command.update request payloadMapper
         | Storage.FileSystem client -> client |> FileSystem.Request.Command.update request payloadMapper
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> Postgre.Request.Command.update request payloadMapper
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
     let updateSeq requests storage =
@@ -108,6 +144,9 @@ module Command =
         match provider with
         | Storage.InMemory client -> client |> InMemory.Request.Command.updateSeq requests payloadMapper
         | Storage.FileSystem client -> client |> FileSystem.Request.Command.updateSeq requests payloadMapper
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> Postgre.Request.Command.updateSeq requests payloadMapper
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
     let createOrUpdate request storage =
@@ -115,6 +154,9 @@ module Command =
         match provider with
         | Storage.InMemory client -> client |> InMemory.Request.Command.createOrUpdate request payloadMapper
         | Storage.FileSystem client -> client |> FileSystem.Request.Command.createOrUpdate request payloadMapper
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> Postgre.Request.Command.createOrUpdate request payloadMapper
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
     let delete id storage =
@@ -122,6 +164,9 @@ module Command =
         match provider with
         | Storage.InMemory client -> client |> InMemory.Request.Command.delete id
         | Storage.FileSystem client -> client |> FileSystem.Request.Command.delete id
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> Postgre.Request.Command.delete id
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
 
     let deleteMany ids storage =
@@ -129,4 +174,7 @@ module Command =
         match provider with
         | Storage.InMemory client -> client |> InMemory.Request.Command.deleteMany ids
         | Storage.FileSystem client -> client |> FileSystem.Request.Command.deleteMany ids
+        | Storage.Database database ->
+            match database with
+            | Database.Client.Postgre client -> client |> Postgre.Request.Command.deleteMany ids
         | _ -> $"The '{provider}' is not supported." |> NotSupported |> Error |> async.Return
