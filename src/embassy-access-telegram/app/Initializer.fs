ba8a353b1dfc279066a6db20712a67f20a1c6a38
Andrei Pestunov <masterlifting@gmail.com>
2025-12-26 11:42:48 +0100

    refactor: Simplify connection string usage in Initializer module

--- src/embassy-access-telegram/app/Initializer.fs
+++ src/embassy-access-telegram/app/Initializer.fs
@@ -3,14 +3,14 @@ module EA.Telegram.Initializer
 open Infrastructure.Prelude
 open AIProvider.Features.DataAccess
 open EA.Core.DataAccess
+open EA.Telegram.Shared
 open EA.Telegram.DataAccess
 
 let private resultAsync = ResultAsyncBuilder()
 let run () =
-    let pgConnectionString =
-        EA.Telegram.Shared.Configuration.ENVIRONMENTS.PostgresConnection
+    let cs = Configuration.ENVIRONMENTS.PostgresConnection
     resultAsync {
-        do! Postgre.Culture.Migrations.apply pgConnectionString
-        do! Postgre.Request.Migrations.apply pgConnectionString
-        return Postgre.Chat.Migrations.apply pgConnectionString
+        do! Postgre.Culture.Migrations.apply cs
+        do! Postgre.Request.Migrations.apply cs
+        return Postgre.Chat.Migrations.apply cs
     }
